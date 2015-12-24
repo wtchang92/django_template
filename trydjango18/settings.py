92b382cffc510e2b66f3929d122dc292d1229a50
wtchang92 <wtchang051592@gmail.com>
2015-12-23 19:22:41 -0500

    2nd commit

--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -153,11 +153,4 @@ if not DEBUG:
     ALLOWED_HOSTS = ['*']
 
     # Static asset configuration
-    import os
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
-
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    
