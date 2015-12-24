66aca1eeea3ad65b8835bc87094b47b7bec959b0
wtchang92 <wtchang051592@gmail.com>
2015-12-24 17:03:57 -0500

    s3 bucket setup 2

--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -145,17 +145,7 @@ LOGIN_REDIRECT_URL = "/"
 if not DEBUG:
 # Parse database configuration from $DATABASE_URL
     import dj_database_url
-    # DATABASES['default'] =  dj_database_url.config()
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'soe_dev',
-            'USER': 'soe_dev_user',
-            'PASSWORD': 'Wtc1992!',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+    DATABASES['default'] =  dj_database_url.config()
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
